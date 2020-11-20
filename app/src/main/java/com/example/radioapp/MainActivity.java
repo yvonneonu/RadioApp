5bcbe6e23f4f8d979f16aa17c01a9e50a1c94780
USER <yvonneonuorah@gmail.com>
2020-11-20 00:33:40 +0000

    RadioApp

--- app/src/main/java/com/example/radioapp/MainActivity.java
+++ app/src/main/java/com/example/radioapp/MainActivity.java
@@ -3,28 +3,56 @@ package com.example.radioapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-import android.widget.ImageButton;
-import android.widget.TextView;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
-    ImageButton bt1;
-    ImageButton bt2;
+   private SimpleExoPlayer player;
+   PlayerView view;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textName);
-        bt1 = findViewById(R.id.buttonPlay);
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        view = findViewById(R.id.radioview);
 
-            }
-        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializePlayer();
+    }
+
+    public void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+        view.setPlayer(player);
+        MediaItem mediaItem = MediaItem.fromUri("http://voa28.akacast.akamaistream.net/7/325/437810/v1/ibb.akacast.akamaistream.net/voa28");
+        player.setMediaItem(mediaItem);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.prepare();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+    public void releasePlayer(){
+        if (player != null){
+            playWhenReady = player.getPlayWhenReady();
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+
+        }
     }
 }
